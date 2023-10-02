d3b05290103e15f711d4c3bb9ed884f9d8809968
Leeyanhawrt <leeyan1108@gmail.com>
2023-10-02 13:59:07 -0400

    Learn about Head component imported from next

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
+
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "@/components/events/event-list";
 
 function HomePage({ featuredEvents }) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   )
